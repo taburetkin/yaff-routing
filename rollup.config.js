fdabb38e2718576d2bc2910b4fefc36a4f6b6e35
dimaver <dima.taburetkin@gmail.com>
2019-10-04 18:37:44 +0300

    libs

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,6 @@ module.exports = [
   {
     input: "./index.js",
     output: {
-      file: "./lib/fe.routing.umd.js",
-      format: "umd",
-      name: "routing"
-    }
-  },
-  {
-    input: "./index.js",
-    output: {
       file: "./lib/fe.routing.esm.js",
       format: "esm"
     }
